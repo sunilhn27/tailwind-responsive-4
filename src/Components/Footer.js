45e082b226a3b47cb7d217e7d7d769991d4ce8c4
Sunil H N <sunilh.n27@gmail.com>
2023-09-15 00:19:50 +0530

    made footer responsive and minor changes in NavBar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,15 +5,15 @@ import DarkLogo from '@/asserts/logo.png'
 function Footer() {
   return (
     <section className='bg-[#081828] text-white py-12'>
-      <div className='flex  md:flex justify-evenly items-center space-x-7'>
+      <div className='flex flex-col md:flex-row justify-center md:justify-evenly items-center space-x-7'>
 
-        <div className='flex flex-col space-y-3'>
-          <Image src={DarkLogo} alt='pic' />
+        <div className='flex flex-col space-y-3 text-center md:text-start'>
+          <Image src={DarkLogo} alt='pic' className='mx-auto md:mx-0' />
           <h1 className='md:w-[26rem] '>Making the world a better place through constructing elegant hierarchies.</h1>
           <p>Follow Us On:</p>
         </div>
 
-        <div className='flex flex-col   text-start space-y-2'>
+        <div className='flex flex-col  items-start space-y-2 md:mt-0 mt-12 '>
           <h1 className='text-[1.5rem] mb-2'>Solutions</h1>
           <p>Marketing</p>
           <p>Analytics</p>
@@ -21,7 +21,7 @@ function Footer() {
           <p>Insights</p>
         </div>
 
-        <div className='flex flex-col   text-start space-y-2'>
+        <div className='flex flex-col   text-start space-y-2 md:mt-0 mt-12'>
           <h1 className='text-[1.5rem] mb-2 '>Support</h1>
           <p>Pricing</p>
           <p>Documentation</p>
@@ -29,11 +29,11 @@ function Footer() {
           <p>API Status</p>
         </div>
 
-        <div className='flex flex-col  items-start'>
-          <h1 className='text-[1.5rem] mb-2'>Subscribe</h1>
-          <p className='md:w-[18rem]'>Subscribe to our newsletter for the latest
+        <div className='flex flex-col  md:items-start text-center'>
+          <h1 className='text-[1.5rem] mb-2 mx-auto md:mx-0 md:mt-0 mt-12'>Subscribe</h1>
+          <p className='md:w-[18rem] md:text-start'>Subscribe to our newsletter for the latest
             updates</p>
-          <input className='bg-[#757575] md:w-[21rem] md:h-[3rem] rounded-lg mt-6 px-3 placeholder:text-gray-300' placeholder='Email Address' />
+          <input className='bg-[#757575] md:w-[21rem] w-[18rem] h-[3.6rem] md:h-[3rem] rounded-lg mt-6 px-3 placeholder:text-gray-300 mx-auto' placeholder='Email Address' />
         </div>
 
       </div>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react'
 import Logo from '@/asserts/logo.png'
+import LogoDark from '@/asserts/dark-logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GiHamburgerMenu } from 'react-icons/gi'
@@ -29,18 +30,20 @@ function NavBar() {
     }, []);
 
     const navbarClass = scrolled ? 'bg-white shadow-md' : 'bg-transparent';
+    const navText = scrolled ? 'text-black' : 'text-red';
 
 
     return (
         <section className='bg-[#2ED06E] py-10'>
             <nav className={` text-[#FFF] flex items-center justify-evenly fixed top-0 left-0 right-0 z-10 p-4 transition-all duration-300 ${navbarClass}`}>
                 <div className='flex justify-between  w-full md:w-[8rem] items-center md:px-0 px-2'>
-                    <div className=' '>
-                        <Image src={Logo} alt='logo' />
+                    <div className={`${mobileView? "mt-8" : "mt-0"}`}>
+                        {scrolled ? (<Image src={LogoDark} alt='logo' className={``}/>) : (<Image src={Logo} alt='logo' className='mt-6' />)}
+
                     </div>
-                    <div className='md:hidden '>
-                        {mobileView ? (<AiOutlineClose className='w-[3rem] h-[1.5rem]' onClick={handleMenu} />) : (<GiHamburgerMenu className='w-[3rem] h-[1.5rem]' onClick={handleMenu} />)}
-                        {mobileView && (<div className='md:hidden flex flex-col absolute top-[3.5rem] right-2 rounded-lg mt-2 space-y-2 bg-white w-[8rem] h-[10rem] px-4 py-2 shadow-lg text-sm text-black'>
+                    <div className={`md:hidden ${scrolled ? "mt-6" : "mt-0"}`}>
+                        {mobileView ? (<AiOutlineClose className={`w-[3rem] h-[1.5rem] ${scrolled? "text-black":""}`} onClick={handleMenu} />) : (<GiHamburgerMenu className={`w-[3rem] h-[1.5rem] ${scrolled? "text-black":""}`} onClick={handleMenu} />)}
+                        {mobileView && (<div className={`md:hidden flex flex-col absolute top-[4.5rem] right-2 rounded-lg mt-2 space-y-2 bg-white w-[8rem] h-[10rem] px-4 py-2 shadow-lg text-sm text-black `}>
                             <Link href={""}>Home</Link>
                             <Link href={""}>About</Link>
                             <Link href={""}>Pages</Link>
@@ -49,7 +52,7 @@ function NavBar() {
                         </div>)}
                     </div>
                 </div>
-                <div className='hidden md:flex gap-6'>
+                <div className={`hidden md:flex gap-6 ${navText}`}>
                     <Link href={""}>Home</Link>
                     <Link href={""}>About</Link>
                     <Link href={""}>Pages</Link>
@@ -57,7 +60,7 @@ function NavBar() {
                     <Link href={""}>Careers</Link>
                 </div>
                 <div className='hidden md:flex'>
-                    <button className='text-black bg-white px-4 py-2 rounded-full'>Try for Free</button>
+                    <button className={`text-black bg-white px-4 py-2 rounded-full ${scrolled ? "bg-green-300" : ""} ${scrolled ? "bg-green-500" : "text-black"}`}>Try for Free</button>
 
                 </div>
             </nav>
