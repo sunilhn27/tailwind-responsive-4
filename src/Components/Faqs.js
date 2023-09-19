2a44a610c917bb388ac01d14e5a91d2d7f8592d5
Sunil H N <sunilh.n27@gmail.com>
2023-09-19 23:45:30 +0530

    Added FAQ Component

--- src/Components/Faqs.js
+++ src/Components/Faqs.js
@@ -1,15 +1,37 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Plus from '@/asserts/Plus.png'
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
+
 function Faqs() {
 
 
 
     const FAQ = ({ }) => {
-        return (<></>)
+
+
+        const [toggle, setToggle] = useState(false);
+
+
+        const handleToggle = () => {
+            setToggle(!toggle);
+        }
+
+        return (<>
+            <div className='w-[22rem] md:w-[39rem] md:h-[3.5rem] bg-white rounded-xl shadow-xl '>
+                <button onClick={handleToggle} className='flex w-[95%] justify-between items-center py-4 mx-2'>
+                    Can I cancel my subscription at anytime?
+                    <span className='flex'>{toggle ? (<AiOutlineMinus />) : (<AiOutlinePlus />)}</span>
+                </button>
+                {toggle && (<div className='leading-relaxed mt-2 px-2 text-start font-sans text-sm py-2'>
+                    It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post,  Ipsum available
+                </div>)}
+            </div>
+        </>)
     }
 
     return (
-        <section className='bg-[#F4F7FA] py-12'>
+        <section className='bg-[#F4F7FA] py-16'>
             <div className='flex flex-col md:flex justify-center items-center'>
                 <div className='flex flex-col text-center space-y-4'>
                     <h3 className='text-[1rem] text-[#2ED06E]'>FAQs</h3>
@@ -18,11 +40,20 @@ function Faqs() {
 
             </div>
 
-            <div className='flex flex-col md:flex justify-center items-center'>
-                <div className='grid md:grid-cols-2'>
+            <div className='flex flex-col md:flex justify-center items-center '>
+                <div className='grid md:grid-cols-2 gap-4'>
                     <div>
-                          
+                        <FAQ />
                     </div>
+                    <div>
+                        <FAQ />
+                    </div>
+
+                    <div>
+                        <FAQ />
+                    </div>
+
+
                 </div>
 
             </div>
